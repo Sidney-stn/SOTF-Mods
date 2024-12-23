a61d7085c7c9dcc78fe8c11ef758e3c215553736
move123456789 <julian.hansen2002@gmail.com>
2024-12-23 05:51:21 +0100

    Sync

--- Items/RegisterItems.cs
+++ Items/RegisterItems.cs
@@ -1,12 +1,12 @@
-﻿using HarmonyLib;
-using RedLoader;
+﻿using RedLoader;
+using RedLoader.NativeUtils;
 using RedLoader.Utils;
 using Sons.Inventory;
 using Sons.Items.Core;
 using SonsSdk;
-using TheForest.Items.Special;
-using TheForest.Utils;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 namespace Signs.Items
 {
@@ -53,52 +53,112 @@ namespace Signs.Items
             itemData2._hasVisualVariant = true;
             itemData2._hasFirstLook = false;
             itemData2.MaxAmount = 1;
+            itemData2.UiData._rightClick = ItemUiData.RightClickCommands.None;
+            itemData2.UiData._leftClick = ItemUiData.LeftClickCommands.take;
+            //itemData2.UiData._icon = ICON
+
             //itemData.SetType(Sons.Items.Core.Types.Equippable, Sons.Items.Core.Types.Droppable, Sons.Items.Core.Types.CraftingMaterial, Sons.Items.Core.Types.Craftable);
             //itemData.SetType(Sons.Items.Core.Types.Equippable);
-            itemData.SetType(Sons.Items.Core.Types.Droppable);
+            //itemData.SetType(Sons.Items.Core.Types.Droppable);
+            itemData.SetType(Sons.Items.Core.Types.Edible);
 
-            HeldOnlyItemController controller = LocalPlayer.Transform.GetComponentInChildren<HeldOnlyItemController>();
-            HeldOnlyItemController.ControllerItemData controllerItem = new HeldOnlyItemController.ControllerItemData();
-            controllerItem._animType = HeldOnlyItemController.ControllerItemData.AnimTypes.Log;
-            controllerItem._dropPositionOffset = new Vector3(0, 0, 0);
-            controllerItem._dropSpawnDelay = 0;
-            controllerItem._itemCache = itemData2;
-            controllerItem._itemId = 7511;
-            controllerItem._itemLength = 4;
-            controllerItem._itemMaxLength = 5;
-            controllerItem._itemThickness = 0.5f;
-            controllerItem._useDropOffset = true;
-            controllerItem._held = controller._heldOnlyItems[0].Held;
-            controller._heldOnlyItems.AddItem(controllerItem); // Number 10 in list so [10]
+            GameObject inventoryLayoutItemGroup = ItemTools.GetInventoryLayoutItemGroup(7511).gameObject;
+            InventoryLayoutItemGroup inventoryLayoutItemGroup2 = inventoryLayoutItemGroup.AddComponent<InventoryLayoutItemGroup>();
+
+            // Hook method for your specific instance
+            var hook = new Hook<OnInteractWithItem>(
+                typeof(InventoryLayoutItemGroup).GetMethod("OnInteractWithItem"),
+                (originalMethod, instance, context) => {
+                    // Check if this is our specific instance
+                    if (instance == inventoryLayoutItemGroup2)
+                    {
+                        // Your code here when the specific instance is interacted with
+                        Debug.Log("Our specific item was interacted with!");
+                    }
+
+                    // Call original method
+                    originalMethod(instance, context);
+                }
+            );
+
+            // Define the delegate for the hook
+            delegate void OnInteractWithItem(InventoryLayoutItemGroup instance, InputAction.CallbackContext context);
+
+
+
+
+            //HeldOnlyItemController controller = LocalPlayer.Transform.GetComponentInChildren<HeldOnlyItemController>();
+            //HeldOnlyItemController.ControllerItemData controllerItem = new HeldOnlyItemController.ControllerItemData();
+            //controllerItem._animType = HeldOnlyItemController.ControllerItemData.AnimTypes.Log;
+            //controllerItem._dropPositionOffset = new Vector3(0, 0, 0);
+            //controllerItem._dropSpawnDelay = 0;
+            //controllerItem._itemCache = itemData2;
+            //controllerItem._itemId = 7511;
+            //controllerItem._itemLength = 4;
+            //controllerItem._itemMaxLength = 5;
+            //controllerItem._itemThickness = 0.5f;
+            //controllerItem._useDropOffset = true;
+            //controllerItem._held = controller._heldOnlyItems[0].Held;
+            //controller._heldOnlyItems.AddItem(controllerItem); // Number 10 in list so [10]
 
 
             // SetupCustomInteraction();
-            SetupCustomInteraction();
+            //SetupCustomInteraction();
 
 
         }
-        public static void SetupCustomInteraction()
-        {
-            Misc.Msg("Injecting Custom Interaction");
-            // Register our class to implement ICustomInventoryItemInteraction
-            InjectorEx.Inject<ICustomInventoryItemInteraction>(typeof(CustomItemInteractionHandler));
+        //public static void SetupCustomInteraction()
+        //{
+        //    Misc.Msg("Starting Custom Interaction Setup");
 
-            // Now we can add it to the GameObject
-            GameObject inventoryLayoutItemGroup = ItemTools.GetInventoryLayoutItemGroup(7511).gameObject;
-            var handler = inventoryLayoutItemGroup.AddComponent<CustomItemInteractionHandler>();
-            var handler2 = inventoryLayoutItemGroup.transform.GetParent().gameObject.AddComponent<CustomItemInteractionHandler>();
-        }
+        //    // First register with ClassInjector
+        //    try
+        //    {
+        //        ClassInjector.RegisterTypeInIl2Cpp<CustomItemInteractionHandler>();
+        //        Misc.Msg("Registered class with IL2CPP");
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Misc.Msg($"Failed to register with IL2CPP: {e}");
+        //        return;
+        //    }
+
+        //    // Then inject the interface
+        //    try
+        //    {
+        //        InjectorEx.Inject<ICustomInventoryItemInteraction>(typeof(CustomItemInteractionHandler));
+        //        Misc.Msg("Injected interface implementation");
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Misc.Msg($"Failed to inject interface: {e}");
+        //        return;
+        //    }
+
+        //    // Finally add component to GameObject
+        //    try
+        //    {
+        //        GameObject inventoryLayoutItemGroup = ItemTools.GetInventoryLayoutItemGroup(7511).gameObject;
+        //        var handler = inventoryLayoutItemGroup.AddComponent<CustomItemInteractionHandler>();
+        //        Misc.Msg("Added component successfully");
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Misc.Msg($"Failed to add component: {e}");
+        //    }
+        //}
     }
 
+    //public class CustomItemInteractionHandler : MonoBehaviour
+    //{
+    //    // Need to add constructor for IL2CPP
+    //    public CustomItemInteractionHandler(IntPtr ptr) : base(ptr) { }
 
-    [RegisterTypeInIl2Cpp]
-    public class CustomItemInteractionHandler : MonoBehaviour
-    {
-        public bool TryPerformAction(ItemInstance itemInstance, bool isUnique)
-        {
-            Misc.Msg($"Custom interaction with item: {itemInstance.Data.Id}");
-            return true;
-        }
-    }
+    //    public bool TryPerformAction(ItemInstance itemInstance, bool isUnique)
+    //    {
+    //        Misc.Msg($"Custom interaction with item: {itemInstance.Data.Id}");
+    //        return true;
+    //    }
+    //}
 
 }
