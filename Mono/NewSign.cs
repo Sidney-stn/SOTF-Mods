dcc329cfdb19bb025d34967e6527f071ad2432e2
move123456789 <julian.hansen2002@gmail.com>
2025-01-27 22:56:46 +0100

    Placing Structure Now Works

---
+++ Mono/NewSign.cs
@@ -0,0 +1,63 @@
+﻿using RedLoader;
+using Sons.Crafting.Structures;
+using UnityEngine;
+
+namespace Signs.Mono
+{
+    [RegisterTypeInIl2Cpp]
+    internal class NewSign : MonoBehaviour
+    {
+        public bool IsPlaceHolder = false;
+
+        private void Start()
+        {
+            if (IsPlaceHolder) { return; }
+            // This Only Gets Called When A New Completed Sign Has Been Created
+            Misc.Msg("[NewSign] Start");
+            Misc.Msg("[NewSign] Deleting Bolt And ScrewStructure");
+            ScrewStructure scewStructure = gameObject.GetComponent<ScrewStructure>();
+            if (scewStructure != null) { DestroyImmediate(scewStructure); }
+            BoltEntity bolt = gameObject.GetComponent<BoltEntity>();
+            if (bolt != null) { DestroyImmediate(bolt); }
+
+            if (gameObject != null)
+            {
+                Misc.Msg("[NewSign] Adding Components");
+                Mono.SignController signController = gameObject.AddComponent<Mono.SignController>();
+                Mono.DestroyOnC destroyOnC = gameObject.AddComponent<Mono.DestroyOnC>();
+
+                signController.SetLineText(1, $"Press {Config.ToggleMenuKey.Value.ToUpper()}");
+                signController.SetLineText(2, "To Edit");
+                signController.SetLineText(3, "Sign");
+                signController.SetLineText(4, "");
+
+                string uniqueId = Guid.NewGuid().ToString();
+
+                signController.UniqueId = uniqueId;
+
+                Saving.Load.ModdedSigns.Add(gameObject);
+                Prefab.SignPrefab.spawnedSigns.Add(signController.UniqueId, gameObject);
+
+                if (Misc.hostMode == Misc.SimpleSaveGameType.Multiplayer || Misc.hostMode == Misc.SimpleSaveGameType.MultiplayerClient)
+                {
+                    Misc.Msg("[NewSign] Spawning On Multiplayer");
+                    (ulong steamId, string stringSteamId) = Misc.MySteamId();
+                    SimpleNetworkEvents.EventDispatcher.RaiseEvent(new Network.SpawnSingeSign
+                    {
+                        Vector3Position = Network.CustomSerializable.Vector3ToString((Vector3)signController.GetPos()),
+                        QuaternionRotation = Network.CustomSerializable.QuaternionToString((Quaternion)signController.GetCurrentRotation()),
+                        UniqueId = uniqueId,
+                        Sender = stringSteamId,
+                        SenderName = Misc.GetLocalPlayerUsername(),
+                        Line1Text = signController.GetLineText(1),
+                        Line2Text = signController.GetLineText(2),
+                        Line3Text = signController.GetLineText(3),
+                        Line4Text = signController.GetLineText(4),
+                        ToSteamId = "None"
+                    });
+                }
+            }
+            else { Misc.Msg("[OnStructureCompleted] signChild == null"); }
+        }
+    }
+}
--- Structure/Setup.cs
+++ Structure/Setup.cs
@@ -8,6 +8,7 @@ namespace Signs.Structure
     internal class Setup
     {
         internal static GameObject signStructure;
+        internal static GameObject signStructurePrefab;
         internal const int signStructureId = 751100;
         internal static void Crafting()
         {
@@ -17,8 +18,8 @@ namespace Signs.Structure
             signStructure = GameObject.Instantiate(Assets.SignObj);
             signStructure.hideFlags = HideFlags.HideAndDontSave;
 
-            Mono.SignController signMono = signStructure.AddComponent<Mono.SignController>();
-            Mono.DestroyOnC destroyOnC = signStructure.AddComponent<Mono.DestroyOnC>();
+            //Mono.SignController signMono = signStructure.AddComponent<Mono.SignController>();
+            //Mono.DestroyOnC destroyOnC = signStructure.AddComponent<Mono.DestroyOnC>();
 
             if (signStructure == null) { Misc.Msg("[Setup] signStructure == null!"); return; }
 
@@ -37,6 +38,17 @@ namespace Signs.Structure
 
             CustomBlueprintManager.TryRegister(new ScrewStructureRegistration(signStructure, signStructureId, "SignRecipie"));
             CustomBlueprintManager.OnCraftingNodeCreated.Subscribe(OnCraftingNodeCreated);
+
+            // Setup The In World Built Prefab
+            PlacementPrefab();
+        }
+
+        private static void PlacementPrefab()
+        {
+            signStructurePrefab = GameObject.Instantiate(Assets.SignObj);
+            signStructurePrefab.hideFlags = HideFlags.HideAndDontSave;
+            Mono.NewSign newSign = signStructurePrefab.AddComponent<Mono.NewSign>();
+            newSign.IsPlaceHolder = true;
         }
 
         internal static void OnCraftingNodeCreated(StructureCraftingNode arg1)
@@ -45,16 +57,58 @@ namespace Signs.Structure
             if (arg1.Recipe.Id == signStructureId)
             {
                 Misc.Msg("Adding OnStructureComplete Event");
+                // Store the GameObject reference
+                var gameObject = arg1.gameObject;
+
                 arg1.StructureCraftingSystem.OnStructureComplete += DelegateSupport.ConvertDelegate<Il2CppSystem.Action>(
-                    new System.Action(OnStructureCompleted)
+                    new System.Action(() => OnStructureCompleted(gameObject))
                 );
 
+                arg1.Recipe.SetCompletedStructurePrefab(signStructurePrefab);
             }
         }
 
-        private static void OnStructureCompleted()
+        private static void OnStructureCompleted(UnityEngine.GameObject gameObject)
         {
-            Misc.Msg("OnStructureCompleted");
+            Misc.Msg($"OnStructureCompleted for GameObject: {gameObject.name}");
+            // You can now access the GameObject and its components here
+            //GameObject signChild = gameObject.transform.GetChild(2).gameObject;
+            //if (signChild != null)
+            //{
+            //    Mono.SignController signController = signChild.AddComponent<Mono.SignController>();
+            //    Mono.DestroyOnC destroyOnC = signChild.AddComponent<Mono.DestroyOnC>();
+
+            //    signController.SetLineText(1, $"Press {Config.ToggleMenuKey.Value.ToUpper()}");
+            //    signController.SetLineText(2, "To Edit");
+            //    signController.SetLineText(3, "Sign");
+            //    signController.SetLineText(4, "");
+
+            //    string uniqueId = Guid.NewGuid().ToString();
+
+            //    signController.UniqueId = uniqueId;
+
+            //    Saving.Load.ModdedSigns.Add(signChild);
+            //    Prefab.SignPrefab.spawnedSigns.Add(signController.UniqueId, signChild);
+
+            //    if (Misc.hostMode == Misc.SimpleSaveGameType.Multiplayer || Misc.hostMode == Misc.SimpleSaveGameType.MultiplayerClient)
+            //    {
+            //        Misc.Msg("Multiplayer Sign Spawned");
+            //        (ulong steamId, string stringSteamId) = Misc.MySteamId();
+            //        SimpleNetworkEvents.EventDispatcher.RaiseEvent(new Network.SpawnSingeSign
+            //        {
+            //            Vector3Position = Network.CustomSerializable.Vector3ToString((Vector3)signController.GetPos()),
+            //            QuaternionRotation = Network.CustomSerializable.QuaternionToString((Quaternion)signController.GetCurrentRotation()),
+            //            UniqueId = uniqueId,
+            //            Sender = stringSteamId,
+            //            SenderName = Misc.GetLocalPlayerUsername(),
+            //            Line1Text = signController.GetLineText(1),
+            //            Line2Text = signController.GetLineText(2),
+            //            Line3Text = signController.GetLineText(3),
+            //            Line4Text = signController.GetLineText(4),
+            //            ToSteamId = "None"
+            //        });
+            //    }
+            //} else { Misc.Msg("[OnStructureCompleted] signChild == null"); }
         }
 
 
